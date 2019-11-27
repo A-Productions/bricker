bc9f65387200e7eb58bd029681164c10e40bfaed
“bblanimation” <cgear13@gmail.com>
2019-11-26 18:43:44 -0600

    preparing for 1.7.1 release

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "blender"     : (2, 80, 0),
     "description" : "Turn any mesh into a 3D brick sculpture or simulation with the click of a button",
     "location"    : "View3D > Tools > Bricker",
-    "warning"     : "Untested Beta release – update to official release when available",  # used for warning icon and text in addons panel
+    "warning"     : "",#Untested Beta release – update to official release when available",  # used for warning icon and text in addons panel
     "wiki_url"    : "https://www.blendermarket.com/products/bricker/",
     "tracker_url" : "https://github.com/bblanimation/bricker/issues",
     "category"    : "Object"}
