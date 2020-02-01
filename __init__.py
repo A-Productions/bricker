4e0dc81f3bc98b04da3ba31fedeefc0112e5f673
“bblanimation” <cgear13@gmail.com>
2020-02-01 10:45:33 -0500

    moved Blender version to 2.82

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ bl_info = {
     "name"        : "Bricker",
     "author"      : "Christopher Gearhart <chris@bblanimation.com>",
     "version"     : (1, 7, 3),
-    "blender"     : (2, 81, 0),
+    "blender"     : (2, 82, 0),
     "description" : "Turn any mesh into a 3D brick sculpture or simulation with the click of a button",
     "location"    : "View3D > Tools > Bricker",
     "warning"     : "Untested Beta release – update to official release when available",  # used for warning icon and text in addons panel
